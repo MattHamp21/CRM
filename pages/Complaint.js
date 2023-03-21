4ce3fe2ae9697d8574c314867ac9cd991233b650
Matt Hamp <Matthewhamp3@gmail.com>
2023-03-21 09:08:11 -0600

    finsihed

--- pages/Complaint.js
+++ pages/Complaint.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import CustomerList from '@/components/CustomerInfo';
-import CrudCustomer from '@/components/CrudCustomer';
+
 import ComplaintCard from '@/components/complaintCard';
-import Link from 'next/link';
 import NavBar from '@/components/NavBar';
 import useAuth from '@/components/useAuth';
 import React, { useEffect } from 'react';
